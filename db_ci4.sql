6627b033b3fc0c6595a206aa614da4d738b3daeb
fahrl <fahrisyaifullah024@gmail.com>
2025-06-11 13:55:00 +0700

    update database

---
+++ db_ci4.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 11 Jun 2025 pada 08.51
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_ci4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+-- Kesalahan membaca struktur untuk tabel db_ci4.migrations: #1932 - Table &#039;db_ci4.migrations&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_ci4.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_ci4`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `product`
+--
+-- Kesalahan membaca struktur untuk tabel db_ci4.product: #1932 - Table &#039;db_ci4.product&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_ci4.product: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_ci4`.`product`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `product_category`
+--
+-- Kesalahan membaca struktur untuk tabel db_ci4.product_category: #1932 - Table &#039;db_ci4.product_category&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_ci4.product_category: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_ci4`.`product_category`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaction`
+--
+-- Kesalahan membaca struktur untuk tabel db_ci4.transaction: #1932 - Table &#039;db_ci4.transaction&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_ci4.transaction: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_ci4`.`transaction`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaction_detail`
+--
+-- Kesalahan membaca struktur untuk tabel db_ci4.transaction_detail: #1932 - Table &#039;db_ci4.transaction_detail&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_ci4.transaction_detail: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_ci4`.`transaction_detail`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+-- Kesalahan membaca struktur untuk tabel db_ci4.user: #1932 - Table &#039;db_ci4.user&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_ci4.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_ci4`.`user`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
